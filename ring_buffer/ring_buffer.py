3492ce1f7588637eab18578e3ea087a86140940a
Enoka Jaona <ejaona@gmail.com>
2019-09-20 12:41:22 -0500

    almost done with ring buffer. Cant get it to pass the last test

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,24 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+      if self.current == self.capacity:
+        # self.current = 0
+        # self.storage[self.current] = item
+        # self.current += 1
+        self.storage[0] = item
+
+        print('full', self.storage)
+      else:
+        self.storage[self.current] = item
+        self.current += 1
+
 
   def get(self):
-    pass
+      print('storage not full',self.storage[: self.current])
+      return self.storage[: self.current]
+    # if self.storage[self.current] == None:
+    #   print('storage not full',self.storage[: self.current])
+    #   return self.storage[: self.current]
+    # else:
+    #   print('storage full', self.storage[: self.capacity])
+    #   # return self.storage[: self.capacity]
